2a81b956593a7c217b2b9d5e29caa83bd8deaa5a
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-25 22:58:55 -0300

    Feat: Resolvendo Exercicio 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+ const employeesFirstName = data.employees.find((name) => name.firstName === employeeName)
+ const employeesLastName = data.employees.find((name) => name.lastName === employeeName)
+
+ 
+  if (employeesFirstName !== undefined) {
+    return employeesFirstName
+  } if (employeesLastName !== undefined) {
+    return employeesLastName
+  }  if (employeesFirstName === undefined || employeesLastName === undefined) {
+    return {}
+  }
 };
 
 module.exports = getEmployeeByName;
