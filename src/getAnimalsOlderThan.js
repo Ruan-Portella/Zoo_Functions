cc470a02d9e54f191ee56c02eb243d3330270757
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-25 21:35:27 -0300

    Feat: Resolvendo Exercicio 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animals = data.species.find((animals) => animals.name === animal)
+  const ages = animals.residents.every((ages) => ages.age >= age)
+  return ages
 };
-
+getAnimalsOlderThan('otters', '7')
 module.exports = getAnimalsOlderThan;
