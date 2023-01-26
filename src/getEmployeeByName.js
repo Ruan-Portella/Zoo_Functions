cb1c878e3fcdedf52395327812c70cd4d78f75f9
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-26 16:22:20 -0300

    Feat: Adicicionando Lens

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,16 +1,15 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
- const employeesFirstName = data.employees.find((name) => name.firstName === employeeName)
- const employeesLastName = data.employees.find((name) => name.lastName === employeeName)
+  const employeesFirstName = data.employees.find((name) => name.firstName === employeeName);
+  const employeesLastName = data.employees.find((name) => name.lastName === employeeName);
 
- 
   if (employeesFirstName !== undefined) {
-    return employeesFirstName
+    return employeesFirstName;
   } if (employeesLastName !== undefined) {
-    return employeesLastName
-  }  if (employeesFirstName === undefined || employeesLastName === undefined) {
-    return {}
+    return employeesLastName;
+  } if (employeesFirstName === undefined || employeesLastName === undefined) {
+    return {};
   }
 };
 
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,29 +1,15 @@
-const { species } = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (id1, id2) => {
   const speciesId = data.species.find((identify) => identify.id === id1);
   const speciesId2 = data.species.find((identify2) => identify2.id === id2);
-  
-  if(speciesId === undefined){
+
+  if (speciesId === undefined) {
     return [];
-  } if (speciesId !== undefined && speciesId2 === undefined){
+  } if (speciesId !== undefined && speciesId2 === undefined) {
     return [speciesId];
-} else {
+  }
   return [speciesId, speciesId2];
-}
-}
-// const getSpeciesByIds = (ids) => {
-//   const speciesId = data.species.find((identify) => identify.id === ids);
-//   if (ids === undefined) {
-//     return [];
-//   } if (ids.length <= 36) {
-//     return [speciesId]
-//   } if (ids.length >= 37) {
-//     const speciesId2 = species.id === ids
-//     return [speciesId2]
-//   } 
-// }
-
+};
 module.exports = getSpeciesByIds;
-
