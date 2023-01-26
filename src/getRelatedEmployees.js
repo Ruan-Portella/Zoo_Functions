629276d3da682c60e5f96a38dc52344630e99b81
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-26 01:05:39 -0300

    Feat: Resolvendo Exercicio 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+ const manager = data.employees.some((ID) => ID.managers.includes(id));
+ return manager
 };
 
+
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
-};
+  const managers = [];
+  if (isManager(managerId) === true) {
+    const managers2 = data.employees.forEach((manage) => {
+      if (manage.managers.includes(managerId)) {
+        managers.push(`${manage.firstName} ${manage.lastName}`);
+      }
+    });
+  } else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return managers
+}; 
+
 
 module.exports = { isManager, getRelatedEmployees };
