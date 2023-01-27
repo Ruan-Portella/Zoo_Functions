72ce50c3a782fa7cd82d32035581514d28bd02a9
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-26 22:36:57 -0300

    Feat: Resolvendo Exercicio 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  const pets = data.species.find((pet) => pet);
-  return pets;
-};
+  if (animal === undefined) {
+    const totalSpecies = data.species.reduce((acc, curr) =>
+      ({ ...acc, [curr.name]: curr.residents.length }), {});
+    return totalSpecies;
+  }
 
+  if (animal.sex === undefined) {
+    const specie = data.species.filter((pet) =>
+      pet.name === animal.species).map((resident) => resident.residents.length)[0];
+    return specie;
+  }
+
+  if (animal.sex !== undefined && animal !== undefined) {
+    const speciesAndSex = data.species.filter((pet) =>
+      pet.name === animal.species).map((resident) =>
+      resident.residents.filter((sexo) => sexo.sex === animal.sex).length)[0];
+    return speciesAndSex;
+  }
+};
 module.exports = countAnimals;
