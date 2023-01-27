971fa3e6eb7270e93e787faa7931613762f3e9d5
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-27 16:33:02 -0300

    Feat: Resolvendo Exercicio 06 e 07

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,39 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('teste 1', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
+  it('teste 2', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toEqual(expected);
+  });
+  it('teste 3', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toEqual(expected);
+  });
+  it('teste 4', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toBe(expected);
+  });
+  it('teste 5', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toEqual(expected);
+  });
+  it('teste 6', () => {
+    const actual = handlerElephants('availability');
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(actual).toEqual(expected);
+  });
+  it('teste 7', () => {
+    const actual = handlerElephants();
+    const expected = undefined;
+    expect(actual).toEqual(expected);
+  });
+});
