d1697f2b4b1728c6e08a04384a1901a327495eca
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-28 22:10:58 -0300

    Fix: Refatorando Exercicio 09

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,36 +1,32 @@
 const data = require('../data/zoo_data');
 
 const week = {
-  Tuesday: { officeHour: '',
-    exhibition: [] },
-  Wednesday: { officeHour: '',
-    exhibition: [] },
-  Thursday: { officeHour: '',
-    exhibition: [] },
-  Friday: { officeHour: '',
-    exhibition: [] },
-  Saturday: { officeHour: '',
-    exhibition: [] },
-  Sunday: { officeHour: '',
-    exhibition: [] },
-  Monday: { officeHour: '',
-    exhibition: [] },
+  Tuesday: {},
+  Wednesday: {},
+  Thursday: {},
+  Friday: {},
+  Saturday: {},
+  Sunday: {},
+  Monday: {},
 };
 
-week.Monday.officeHour = 'CLOSED';
-week.Monday.exhibition = 'The zoo will be closed!';
 const generos = Object.keys(week);
 
 for (let index = 0; index < generos.length - 1; index += 1) {
   week[generos[index]].officeHour = `Open from ${data.hours[generos[index]]
     .open}am until ${data.hours[generos[index]].close}pm`;
+  week[generos[index]].exhibition = [];
 
   data.species.forEach((specie) => {
     if (specie.availability.includes(generos[index])) {
       week[generos[index]].exhibition.push(specie.name);
     }
   });
+  week.Monday.officeHour = 'CLOSED';
+  week.Monday.exhibition = 'The zoo will be closed!';
 }
+
+console.log(week);
 
 const getSchedule = (scheduleTarget) => {
   for (let index = 0; index < generos.length; index += 1) {
