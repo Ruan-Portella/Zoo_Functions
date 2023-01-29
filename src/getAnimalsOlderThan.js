be4e89db5e1d32e9546cc74eacc083579e5016ed
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-29 18:06:46 -0300

    Fix: Arrumando Lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,10 +1,9 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  const animals = data.species.find((animals) => animals.name === animal)
-  const ages = animals.residents.every((ages) => ages.age >= age)
-  return ages
+  const animals2 = data.species.find((animals) => animals.name === animal);
+  const age1 = animals2.residents.every((age2) => age2.age >= age);
+  return age1;
 };
-getAnimalsOlderThan('otters', '7')
+getAnimalsOlderThan('otters', '7');
 module.exports = getAnimalsOlderThan;
