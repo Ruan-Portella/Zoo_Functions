93839c754144864e02ea93e01fcc58bb6a6b1341
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-30 15:05:58 -0300

    Feat: resolvendo exercicio 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,29 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('teste 1', () => {
+    const actual = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(actual).toEqual(expected);
+  });
+  it('teste 2', () => {
+    const expected = 'The zoo is closed';
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe(expected);
+  });
+  it('teste 3', () => {
+    const expected = 'The zoo is open';
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe(expected);
+  });
+  it('teste 4', () => {
+    const expected = 'The zoo is closed';
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe(expected);
+  });
+});
